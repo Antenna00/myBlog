4ee6f8744aa6f698ccec6ee5d9efc675c969ea2c
RinKit6024 <butasan6024@gmail.com>
2023-12-04 02:17:23 +0900

    refactored and made responsive

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,62 +1,91 @@
+"use client";
+
 import React from "react";
-import Upload from "../components/upload/Upload";
-import Image from "next/image";
+
+//TODO onsubmit時のhandler 
+const onSubmit = () => {
+    // ここからお願いします
+};
 
 //TODO メール送信はnodemailerを使用
-function contact(){
+function contact() {
+  return (
+    //TODO flex1 でxl >画像追加。
+    <div className="mt-14 flex flex-col">
+      <p className="text-3xl text-center flex justify-center font-bold">
+        Let's connect<p className="text-red-400">.</p>
+      </p>
 
-    return(
-        <div className="mt-10">
-            <div className="flex justify-center mb-10">
-                <p className="text-2xl">
-                    Let's connect.
-                </p>
+      <div className="flex justify-center mt-7">
+        <form
+          className="flex flex-col w-full max-w-lg gap-8"
+          onSubmit={onSubmit}
+        >
+          <div className="flex justify-between sm:flex-col gap-5">
+            <div className="w-full flex-1">
+              <label className="block uppercase tracking-wide text-xs font-bold mb-2">
+                name
+              </label>
+              <input
+                className="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-city"
+                type="text"
+                placeholder="Name"
+                required
+              />
             </div>
-            <div className="flex justify-center">
-                <form className="w-full max-w-lg">
-                        <div className="flex flex- -mwrapx-3 mb-2">
-                            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                                    name
-                                </label>
-                                <input className="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="your name" />
-                            </div>
-                            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                                    mail
-                                </label>
-                                <input className="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="your mail" />
-                            </div>
-        
-                        </div>
-                        <div className="flex flex- -mwrapx-3 mb-2">
-                            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                                    Subject
-                                </label>
-                                <input className="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="your subject" />
-                            </div>
-                        </div>
-                        <div className="flex flex- -mwrapx-3 mb-2">
-                            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Message</label>
-                                <textarea id="message" rows={4} className="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" placeholder="your message here...">
-                                    
-                                </textarea>
-                            </div>
-                        </div>
-                        
-                        <div>
-                            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                                <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
-                                    Let's talk
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+            <div className="w-full flex-1">
+              <label className="block uppercase tracking-wide text-xs font-bold mb-2">
+                mail
+              </label>
+              <input
+                className="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-city"
+                type="text"
+                placeholder="Mail"
+                required
+              />
             </div>
-        </div>
-    )
+          </div>
+
+          <div className="w-full">
+            <label className="block uppercase tracking-wide text-xs font-bold mb-2">
+              Subject
+            </label>
+            <input
+              className="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-city"
+              type="text"
+              placeholder="Subject"
+              required
+            />
+          </div>
+
+          <div className="w-full">
+            <label className="block uppercase tracking-wide text-xs font-bold mb-2">
+              Message
+            </label>
+            <textarea
+              id="message"
+              rows={4}
+              className="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              placeholder="Message"
+              required
+            ></textarea>
+          </div>
+
+          <div className="w-full md:w-1/3 mb-6 md:mb-0">
+            <button
+              className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+              type="submit"
+            >
+              Let's talk
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default contact;
